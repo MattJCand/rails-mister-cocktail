948612781c6893dbcae7fcabd737050c02f011eb
mathieu junes <matyjunes@gmail.com>
2017-05-19 17:16:54 +0200

    new version with images gem

--- Gemfile
+++ Gemfile
@@ -48,6 +48,11 @@ gem "font-awesome-sass"
 #gem security
 gem 'figaro'
 
+#gem upload
+gem 'cloudinary', '1.1.7'
+gem 'attachinary', github: 'assembler/attachinary'
+gem 'jquery-fileupload-rails'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,31 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+require 'json'
+require 'open-uri'
+
 puts 'Cleaning database...'
 
+Cocktail.destroy_all
 Ingredient.destroy_all
+Dose.destroy_all
 
-Ingredient.create(name: "lemon")
-Ingredient.create(name: "ice")
-Ingredient.create(name: "mint leaves")
-Cocktail.create(name: "Pina colada")
-Cocktail.create(name: "Mojito")
-Cocktail.create(name: "Americano")
+url = 'http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredient_url= open(url).read
+ingredient = JSON.parse(ingredient_url)
+
+ingredient["drinks"].each do |i|
+  Ingredient.create!(name: i["strIngredient1"])
+end
+
+url_cocktail = 'http://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic'
+cocktail_url= open(url_cocktail).read
+cocktail = JSON.parse(cocktail_url)
+
+cocktail["drinks"].first(12).each do |i|
+  c = Cocktail.new(name: i["strDrink"])
+  c.photo_url = i["strDrinkThumb"]
+  c.save
+end
 
 puts 'Finished'
